e816a6c6a31067e5b4b2cd22ca97644e6c539429
Teddy Chambard <teddy.chambard@sage.com>
2016-12-09 09:28:56 +0100

    remove unnecessary require

--- app.ts
+++ app.ts
@@ -1,8 +1,6 @@
 import { _ } from 'streamline-runtime';
 import { AdminServer } from 'spirit.io-admin-application/app';
 
-const cookieParser = require('cookie-parser');
-
 export class SampleApp extends AdminServer {
     constructor(config: any) {
         super(config);
