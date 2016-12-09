3d0efcd6a08f249d2e0ff478d7467a05eeba026a
Teddy Chambard <teddy.chambard@sage.com>
2016-12-09 09:27:31 +0100

    simplify app

--- app.ts
+++ app.ts
@@ -1,4 +1,3 @@
-/// <reference path="node_modules/spirit.io/index.d.ts" />
 import { _ } from 'streamline-runtime';
 import { AdminServer } from 'spirit.io-admin-application/app';
 
@@ -6,13 +5,6 @@ const cookieParser = require('cookie-parser');
 
 export class SampleApp extends AdminServer {
     constructor(config: any) {
-        console.log("Config:", config)
         super(config);
-
-        this.on('initialized', () => {
-            console.log("========== Server initialized ============\n");
-        });
-        console.log("\n========== Initialize server begins ============");
-
     }
 }
